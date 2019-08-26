51298428e3eeeb5e8afb9d8e45696887fb862d50
clmntgbr <clement.goubier@gmail.com>
2019-08-26 08:50:58 +0200

    qzd

--- ryad.go
+++ ryad.go
@@ -9,7 +9,7 @@ func main() {
 	if xlFile, err := xls.Open("cables.xls", "utf-8"); err == nil {
 		if sheet1 := xlFile.GetSheet(3); sheet1 != nil {
 			fmt.Print("Total Lines ", sheet1.MaxRow, sheet1.Name)
-			col1 := sheet1.Row(1).Col()
+			col1 := sheet1.Row(1).Col(1)
 			for i := 0; i <= (int(sheet1.MaxRow)); i++ {
 				row1 := sheet1.Row(i)
 				col1 = row1.Col(1)
